452d79db8bd62c6bb2793b26f094b65a25453a18
Moe0088 <you@example.com>
2024-02-15 17:15:05 -0500

    Changes

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -3,8 +3,8 @@ package com.coderscampus;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		generate a random number
+//		and collect user inpt
 	}
 
 }
